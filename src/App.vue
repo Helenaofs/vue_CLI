f9c3ca12f0aaa736d31d721fa555928839285b2f
helenaofs <lilklinik@yandex.ru>
2023-09-18 23:04:16 +0700

    task added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+  <div>
+  <Header />
+  <router-view/>
+  <Footer />
+</div>
+ </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+ <script>
+ import Header from './components/Header.vue'
+ import Footer from './components/Footer.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+ export default {
+   name: 'App',
+   components: {
+     Header,
+     Footer,
+   }
+ }
+ </script>
